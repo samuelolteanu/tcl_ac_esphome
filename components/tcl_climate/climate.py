8ced9c4df46ca459a9c52aaf0b38a9d611381cc5
samuelolteanu <85267083+samuelolteanu@users.noreply.github.com>
2026-08-30 21:07:38 +0300

    Update climate.py

--- components/tcl_climate/climate.py
+++ components/tcl_climate/climate.py
@@ -1,7 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate, uart
-from esphome.const import CONF_ID
+from esphome.components import climate, uart, sensor
+from esphome.const import CONF_ID, CONF_SENSOR
+
+CONF_HUMIDITY_SENSOR = "humidity_sensor"
 
 DEPENDENCIES = ['uart']
 AUTO_LOAD = ['climate']
@@ -11,6 +13,8 @@ TCLClimate = tcl_climate_ns.class_('TCLClimate', climate.Climate, uart.UARTDevic
 
 CONFIG_SCHEMA = climate.climate_schema(TCLClimate).extend({
     cv.GenerateID(): cv.declare_id(TCLClimate),
+    cv.Optional(CONF_SENSOR): cv.use_id(sensor.Sensor),
+    cv.Optional(CONF_HUMIDITY_SENSOR): cv.use_id(sensor.Sensor),
 }).extend(uart.UART_DEVICE_SCHEMA).extend(cv.polling_component_schema('450ms'))
 
 async def to_code(config):
@@ -18,3 +22,11 @@ async def to_code(config):
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
     await uart.register_uart_device(var, config)
+
+    if CONF_SENSOR in config:
+        sens = await cg.get_variable(config[CONF_SENSOR])
+        cg.add(var.set_temperature_sensor(sens))
+
+    if CONF_HUMIDITY_SENSOR in config:
+        hsens = await cg.get_variable(config[CONF_HUMIDITY_SENSOR])
+        cg.add(var.set_humidity_sensor(hsens))
